a0f6d768ff38e0fbea9b329aba0f9f0f0e4b05be
Artur Klesun <arturklesun@gmail.com>
2020-05-18 01:19:32 +0300

    Fix, do not flood CPU with errors of logs directory missing

--- src/Log.ts
+++ src/Log.ts
@@ -4,12 +4,23 @@ const fs = fsMod.promises;
 
 const Debug = require('klesun-node-tools/src/Debug.js');
 
+let whenLogsDirCheck: Promise<boolean> | null = null;
+
+const logsDirPath = __dirname + '/../out';
 
 const Log = {
-    info: (msgData: any) => {
-        const path = __dirname + '/../out/Debug_log.txt';
-        const formatted = Debug.jsExport(msgData);
-        return fs.appendFile(path, formatted + ',\n');
+    info: async (msgData: any) => {
+        if (whenLogsDirCheck === null) {
+            whenLogsDirCheck = fs.stat(logsDirPath)
+                .then(info => true)
+                .catch(exc => false);
+        }
+        const hasLogsDir = await whenLogsDirCheck;
+        if (hasLogsDir) {
+            const path = logsDirPath + '/Debug_log.txt';
+            const formatted = Debug.jsExport(msgData);
+            await fs.appendFile(path, formatted + ',\n');
+        }
     },
 };
 
